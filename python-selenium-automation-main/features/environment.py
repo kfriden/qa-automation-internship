d50036563dc50598efb7ef5c00bf7602e5088ec3
kfriden <kfriden@verizon.net>
2024-06-04 21:42:30 -0400

    adding ability to test in BrowserStack in the cloud

--- python-selenium-automation-main/features/environment.py
+++ python-selenium-automation-main/features/environment.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
@@ -7,7 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from app.application import Application
 
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
@@ -16,9 +17,9 @@ def browser_init(context):
     # context.driver = webdriver.Chrome(service=service)
 
     # FIREFOX BROWSER
-    driver_path = GeckoDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
 
     # HEADLESS MODE
     # options = webdriver.ChromeOptions()
@@ -26,6 +27,22 @@ def browser_init(context):
     # options.add_argument('--window-size=1920,1080')
     # service = Service(ChromeDriverManager().install())
     # context.driver = webdriver.Chrome(options = options, service = service)
+
+    # BROWSERSTACK CODE #
+    bs_user = 'kaitlynfriden_y2mXKl'
+    bs_key = 'eQHRTGhV6JqHDHTJNyFm'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        'os': 'OS X',
+        'osVersion': 'Monterey',
+        'browserName': 'Edge',
+        'sessionName': scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
 
     context.driver.maximize_window()
 
@@ -39,7 +56,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
